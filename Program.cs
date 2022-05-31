980760214dcea49c3b8c070a49c91f07a06a888f
Pedro Henrique <phdias2004@gmail.com>
2022-05-31 16:56:06 -0300

    Operação subtrair

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 var n1 = Convert.ToDouble(args[1]);
 var n2 = Convert.ToDouble(args[2]);
 
-if(operacao == "Somar"){
+if(operacao == "Somar")
+{
     Console.WriteLine(n1+n2);
+}
+
+if(operacao == "Subtrair")
+{
+    Console.WriteLine(n1-n2);
 }
